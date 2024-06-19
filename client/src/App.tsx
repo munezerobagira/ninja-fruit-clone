3a215498843f7031e9fb9cb1325848eb3e1de54d
Sostene Munezero Bagira <munezerobagira@gmail.com>
2024-06-19 19:16:56 +0200

    fix: typo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,6 @@ import "./App.css";
 import router from "./router";
 import { ThemeProvider } from "@emotion/react";
 import { theme } from "./theme";
-import MenuAppBar from "./components/MenuAppBar";
-import BottomNavigator from "./components/BottomNavigator";
 import { CircularProgress, Stack } from "@mui/material";
 import { WebAppProvider } from "@vkruglikov/react-telegram-web-app";
 
